73280f3b0adfb8b853361dd8126fef641d78f5a0
mjg1000 <43820171+mjg1000@users.noreply.github.com>
2026-02-06 05:55:14 +0000

    Refactor to only allow single decisions to be made (avoid terminal type sub-levels)

--- one_night_werewolf.py
+++ one_night_werewolf.py
@@ -52,11 +52,27 @@ class giveInfo(Node):
         #     if i != self.sequence[0][self.sequence[1]]:
         #         possible.append("claim voting for "+str(i))
         # #12
-        if self.sub_level < 1: # If to deep in current player saying things, dont let them say more
-            for i in possible:
-                self.children.append(giveInfo(i, self.sequence, self.level, self.sub_level + 1, self.gs))
-        
-        self.children.append(terminalInfo("finished claims", self.sequence, self.level, self.sub_level + 1, self.gs)) # Done with claims 
+        new_seq = copy.deepcopy(self.sequence)
+        new_seq[1] += 1 
+        new_level = self.level
+        if new_seq[1] >= len(new_seq[0]): # this was the final node in the subsequence
+            
+            new_seq = [[0,1,2],0]
+            new_gamestate = copy.deepcopy(self.gs)
+            new_gamestate["has claim"][0] = 0
+            new_gamestate["has claim"][1] = 0
+            new_gamestate["has claim"][2] = 0
+            new_level += 1 
+
+            base_class = AdminAction
+            if self.level > MAX_LEVEL:
+                base_class = gameOver
+        else:
+            base_class = giveInfo
+            new_gamestate = copy.deepcopy(self.gs)
+
+        for i in possible:
+            self.children.append(base_class(i, new_seq, new_level, self.sub_level + 1, new_gamestate))
         #11
         if len(self.children) > 1:
             children2 = [] 
@@ -99,7 +115,7 @@ class terminalInfo(giveInfo):
         new_seq[1] += 1 
         if new_seq[1] >= len(new_seq[0]): # this was the final node in the subsequence
             if self.level > MAX_LEVEL:
-                self.children = [[gameOver(self.sequence, self.level, self.sub_level, self.gs),1] ]
+                self.children = [[gameOver(self.sequence, self.level, self.sub_level, self.gs),1]]
             else:
                 new_seq = [[0,1,2],0]
                 new_gamestate = copy.deepcopy(self.gs)
@@ -129,46 +145,6 @@ class AdminAction(Node):
         """possible.append("set happy to vote")"""
         #TODO: change this to options: Have claim + happy to vote. Have claim + ¬happy to vote. ¬Have claim + happy to vote. ¬Have claim + ¬happy to vote. All should have terminal admin as children
         #TODO: Get rid of terminal admin and just directly point to next player node 
-        if self.sub_level < 1:
-            new_game_state = copy.deepcopy(self.gs)
-            new_game_state["ready to vote"][self.sequence[1]] = 1 
-            self.children.append(AdminAction("set happy to vote", self.sequence, self.level, self.sub_level+1, copy.deepcopy(new_game_state)))
-
-            """possible.append("set not happy to vote")"""
-            new_game_state["ready to vote"][self.sequence[1]] = 0
-            self.children.append(AdminAction("set not happy to vote", self.sequence, self.level, self.sub_level+1, copy.deepcopy(new_game_state)))
-        
-        
-            """possible.append("has claim")"""
-            new_game_state = copy.deepcopy(self.gs)
-            new_game_state["has claim"][self.sequence[1]] = 1
-            self.children.append(AdminAction("has claim", self.sequence, self.level, self.sub_level+1, copy.deepcopy(new_game_state)))
-
-        self.children.append(terminalAdmin("has claim", self.sequence, self.level, self.sub_level+1, self.gs))
-        #4 
-        if len(self.children) > 1:
-            children2 = [] 
-            for start in STARTS:
-                start_children = []
-                for i in self.children:
-                    likelihood = 1/(len(self.children))
-                    start_children.append([i, likelihood])
-                children2.append(start_children)
-            self.children = children2
-
-        
-            for i in self.children[0]:
-                i[0].gen_children() 
-        else:
-            self.children = [[self.children[0],1]]
-            self.children[0][0].gen_children()
-            
-
-class terminalAdmin(AdminAction):
-    """
-    Player has finished with the admin actions they currently want to do, ready to concede to next player or go to claim making procedure
-    """
-    def gen_children(self):
         new_seq = copy.deepcopy(self.sequence)
         new_seq[1] += 1 
         if new_seq[1] >= len(new_seq[0]): # this was the final node in the subsequence
@@ -176,29 +152,55 @@ class terminalAdmin(AdminAction):
             for i in range(PLAYER_COUNT):
                 if self.gs["has claim"][i] == 1:
                     for_seq.append(i)
+            
+            # first nodes where don't want to make claim
+            new_game_state = copy.deepcopy(self.gs)
+            new_game_state["has claim"][self.sequence[1]] = 0
             if len(for_seq) == 0:
                 if self.level > MAX_LEVEL: # If done reached stack limit and nobody has claims then end
-                    self.children = [[gameOver(self.sequence, self.level, self.sub_level, self.gs),1]]
+                    self.children.append(gameOver("end", self.sequence, self.level, self.sub_level, copy.deepcopy(new_game_state)))
                 else:
-                    new_seq = [[0,1,2],0]
-                    new_gamestate = copy.deepcopy(self.gs)
-                    new_gamestate["has claim"][0] = 0
-                    new_gamestate["has claim"][1] = 0
-                    new_gamestate["has claim"][2] = 0
-                    self.children = [[AdminAction("no claims; conceded to player 0",new_seq, self.level+1, 0, new_gamestate), 1]]
-
+                    new_seq = [[0,1,2],0] # ascending order default for admin actions as order shouldnt matter 
+                    self.children.append(AdminAction("no claims; conceded to player 0",new_seq, self.level+1, 0, copy.deepcopy(new_game_state)))
             else:
-                # perms = list(permutations(for_seq, len(for_seq)))
-                # for i in perms: #n!
-                #     new_seq = [list(i), 0]
-                #     self.children.append([giveInfo("claim loop started; conceded to " + str(new_seq[0][new_seq[1]]), new_seq, self.level+1, 0, self.gs), 1/len(perms)])
-                
-                #Only [0,1,2] order to preserve time 
-                self.children = [[giveInfo("claim loop started; conceded to " + str(0), [[0,1,2],0], self.level+1, 0, self.gs), 1]]
-        else:
-            self.children = [[AdminAction("conceded to player " + str(new_seq[1]),new_seq, self.level, 0, self.gs), 1]]
+                self.children = [giveInfo("claim loop started; conceded to player " + str(for_seq[0]), [for_seq, for_seq[0]], self.level+1, 0, self.gs)]
 
-        self.children[0][0].gen_children()
+            # Now nodes where do want to make a claim 
+            for_seq.append(2) # This must be player num 2
+            new_game_state["has claim"][self.sequence[1]] = 1
+            new_game_state["ready to vote"][self.sequence[1]] = 1 
+            self.children.append(giveInfo("has claim and ready to vote. claim loop started; conceded to player " + str(for_seq[0]), [for_seq, for_seq[0]], self.level+1, 0, copy.deepcopy(new_game_state)))
+            new_game_state["ready to vote"][self.sequence[1]] = 0
+            self.children.append(giveInfo("has claim and not ready to vote. claim loop started; conceded to player " + str(for_seq[0]), [for_seq, for_seq[0]], self.level+1, 0, copy.deepcopy(new_game_state)))
+        else:
+            new_game_state = copy.deepcopy(self.gs)
+            new_game_state["ready to vote"][self.sequence[1]] = 1 
+            new_game_state["has claim"][self.sequence[1]] = 1
+            self.children.append(AdminAction("set happy to vote, has claim", new_seq, self.level, self.sub_level, copy.deepcopy(new_game_state)))
+            new_game_state["has claim"][self.sequence[1]] = 0
+            self.children.append(AdminAction("set happy to vote, hasnt claim", new_seq, self.level, self.sub_level, copy.deepcopy(new_game_state)))
+
+            
+            new_game_state["ready to vote"][self.sequence[1]] = 0 
+            new_game_state["has claim"][self.sequence[1]] = 1
+            self.children.append(AdminAction("not happy to vote, has claim", new_seq, self.level, self.sub_level, copy.deepcopy(new_game_state)))
+            new_game_state["has claim"][self.sequence[1]] = 0
+            self.children.append(AdminAction("not happy to vote, hasnt claim", new_seq, self.level, self.sub_level, copy.deepcopy(new_game_state)))
+
+            
+        #4 
+        children2 = [] 
+        for start in STARTS:
+            start_children = []
+            for i in self.children:
+                likelihood = 1/(len(self.children))
+                start_children.append([i, likelihood])
+            children2.append(start_children)
+        self.children = children2
+
+        for i in self.children[0]:
+            i[0].gen_children() 
+            
 
 class vote(Node):
     def __init__(self, sequence, level, sub_level, gs, player, type = "started"):
@@ -223,7 +225,7 @@ class vote(Node):
         return self.children
 
 class gameOver(Node): 
-    def __init__(self, sequence, level, sub_level, gs, type = "end"):
+    def __init__(self, type, sequence, level, sub_level, gs):
         self.type = type
         self.sub_level = sub_level
         super().__init__( sequence, level, gs)
